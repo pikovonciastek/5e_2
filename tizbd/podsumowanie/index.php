3b1b4ea9574903365d74ded941846057816811a0
Aleksander <72768278+pikovonciastek@users.noreply.github.com>
2025-06-03 10:21:45 +0200

    update

--- tizbd/podsumowanie/index.php
+++ tizbd/podsumowanie/index.php
@@ -1,8 +1,8 @@
 <?php
 $link = new mysqli ("localhost", "root", "", "w3schools");
-$sql = "select * from products";
+$sql = "select * from suppliers";
 $result = $link -> query($sql);
-
+$suppliers = $result -> fetch_all(1);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,14 +15,20 @@ $result = $link -> query($sql);
     <form action="" method="post">
         <label for="supplier">Wybierz dostawcę</label>
         <select name="supplier" id="supplier">
-
+        <?php
+        foreach($suppliers as $supplier){
+            echo "<option value='{$supplier['SupplierID']}'> {$supplier['SupplierName']} </option>";
+        }
+        ?>
         </select>
+
         <br>
         <label for="product">Wpisz nazwę towaru</label>
         <input type="text" name="product" id="product">
         <br>
         <label for="price">Ustaw cenę</label>
-        <input type="range" name="price" id="price" min="0" max="200">
+        <input type="range" name="price" id="price" min="0" max="200" step="0.1">
+        <br>
         <button>Dodaj</button>
     </form>
 </body>
