9129c746d7179eea881850bb80d0aecfc2fc5ba7
Aleksander <72768278+pikovonciastek@users.noreply.github.com>
2025-09-19 17:21:16 +0200

    Update script.js

--- tworzenie stron i aplikacji/zadanie egzaminacyjne 3/script.js
+++ tworzenie stron i aplikacji/zadanie egzaminacyjne 3/script.js
@@ -1,7 +1,36 @@
-let przycisk = document.getElementById('obliczanie');
-let wynik = document.getElementById('wynik');
+let ceny = {
+  "React.js": 1500,
+  "JavaScript": 1200
+};
 
+document.getElementById("obliczanie").onclick = function() {
+  let kursy = "";
+  let kwota = 0;
 
+  if (document.getElementById("react").checked) {
+    kursy += "React.js ";
+    kwota += ceny["React.js"];
+  }
 
-wynik.element.innerHTML = `Kurs odbędzie się w ${miasto}. Koszt całkowity:
-${kwota} zł. Płacisz ${liczba} rat po ${rata} zł`;
+  if (document.getElementById("js").checked) {
+    kursy += "JavaScript ";
+    kwota += ceny["JavaScript"];
+  }
+
+  let ratyElement = document.getElementById("raty");
+  let liczbaRat = ratyElement.getAttribute("value") * 1;
+
+  let miastoSelect = document.getElementById("miasta");
+  let miasto = miastoSelect.options[miastoSelect.selectedIndex].innerHTML;
+
+  let rata = (kwota / liczbaRat) + "";
+  let kropka = rata.indexOf(".");
+  if (kropka !== -1) {
+    rata = rata.substr(0, kropka + 3);
+  }
+
+  let wynik = "Kurs odbędzie się w " + miasto + ". Koszt całkowity: " + kwota + " zł. Płacisz " + liczbaRat + " rat po " + rata + " zł.";
+  document.getElementById("wynik").innerHTML = wynik;
+  document.getElementById("wynik").style.color = "blue";
+  document.getElementById("wynik").style.fontWeight = "bold";
+};
