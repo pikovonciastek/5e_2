1e59beafa12e506255475cfcf7822eafae8f9dc4
Aleksander <72768278+pikovonciastek@users.noreply.github.com>
2025-09-19 09:34:46 +0200

    Add files via upload

---
+++ tworzenie stron i aplikacji/zadanie egzaminacyjne 3/script.js
@@ -0,0 +1,7 @@
+let przycisk = document.getElementById('obliczanie');
+let wynik = document.getElementById('wynik');
+
+
+
+wynik.element.innerHTML = `Kurs odbędzie się w ${miasto}. Koszt całkowity:
+${kwota} zł. Płacisz ${liczba} rat po ${rata} zł`;
